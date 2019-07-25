4768837fe1ecd5fc9833cb0040ec9df551e0bbd9
Apple <apple@Apples-MacBook-Pro-132.local>
2019-07-25 16:44:53 -0400

    first iteration complerte

--- Emojional Project/Emojional Project/ViewController.swift
+++ Emojional Project/Emojional Project/ViewController.swift
@@ -15,6 +15,32 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton) {
+        //when click on rain emoji make and array of four options that will come up in an allert
+        //when click on sun emoji make an array of three optons that will come up in an allert
+        let alertControllerRain = UIAlertController(title: "Rainy Day?", message: "Get inside and stay dry!", preferredStyle: UIAlertController.Style.alert)
+        alertControllerRain.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertControllerRain, animated: true, completion: nil)
+        
+        let alertControllerSun = UIAlertController(title: "It's a beautiful day!", message: "What are you still doing instide?!", preferredStyle: UIAlertController.Style.alert)
+        alertControllerSun.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertControllerSun, animated: true, completion: nil)
 
+    }
+    
+    @IBAction func showMessageRain(sender: UIButton) {
+        //when click on rain emoji make and array of four options that will come up in an allert
+        //when click on sun emoji make an array of three optons that will come up in an allert
+        let alertControllerRain = UIAlertController(title: "Rainy Day?", message: "Get inside and stay dry!", preferredStyle: UIAlertController.Style.alert)
+        alertControllerRain.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertControllerRain, animated: true, completion: nil)
+    }
+    
+    @IBAction func showMessageSun(sender: UIButton){
+        let alertControllerSun = UIAlertController(title: "It's a beautiful day!", message: "What are you still doing instide?!", preferredStyle: UIAlertController.Style.alert)
+        alertControllerSun.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertControllerSun, animated: true, completion: nil)
+    }
+    let 
 }
 
