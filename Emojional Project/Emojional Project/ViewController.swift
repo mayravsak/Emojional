4134645ba42297845b76117643d936e3fbaba07b
Apple <apple@Apples-MacBook-Pro-132.local>
2019-07-26 11:36:29 -0400

    added 4 more emojis

--- Emojional Project/Emojional Project/ViewController.swift
+++ Emojional Project/Emojional Project/ViewController.swift
@@ -15,27 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    //The first two bottons together but not useful because i separated them:
-//    @IBAction func showMessage(sender: UIButton) {
-//        //when click on rain emoji make and array of four options that will come up in an allert
-//        //when click on sun emoji make an array of three optons that will come up in an allert
-//        let alertControllerRain = UIAlertController(title: "Rainy Day?", message: "Get inside and stay dry!", preferredStyle: UIAlertController.Style.alert)
-//        alertControllerRain.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-//        present(alertControllerRain, animated: true, completion: nil)
-//
-//        let alertControllerSun = UIAlertController(title: "It's a beautiful day!", message: "What are you still doing instide?!", preferredStyle: UIAlertController.Style.alert)
-//        alertControllerSun.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-//        present(alertControllerSun, animated: true, completion: nil)
-//
-//    }
-    
     let customMessagesRain  = [
-        "🌧": ["train your pet rock", "build a blanket fort", "play hide and seek with leora's friends", "bake a cake"]
+        "🌧": ["Train his pet rock and you should too!", "Building a blanket fort and you should too!", "Looking at memes in instagram and you should too!", "Baking a cake and you should too!"]
     ]
     @IBAction func showMessageRain(sender: UIButton) {
         let selectedEmoji = sender.titleLabel?.text
         let emojiMessageRain = customMessagesRain[selectedEmoji!]?.randomElement()
-        let alertControllerRain = UIAlertController(title: "Rainy Day?", message: emojiMessageRain, preferredStyle: UIAlertController.Style.alert)
+        let alertControllerRain = UIAlertController(title: "It's Raining, It's Pouring, The Old Man Is", message: emojiMessageRain, preferredStyle: UIAlertController.Style.alert)
         
         alertControllerRain.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertControllerRain, animated: true, completion: nil)
@@ -43,7 +29,7 @@ class ViewController: UIViewController {
     
     
     let customMessagesSun = [
-     "☀️": ["run a marathon", "What are you still doing instide?!", "have a photoshoot", "find a beach and go swim in the ocean", "start a garden"]
+     "☀️": ["Run a marathon", "What are you still doing instide?!", "Have a photoshoot", "Find a beach and go swim in the ocean", "Start a garden"]
     ]
     @IBAction func showMessageSun(sender: UIButton){
         let selectedEmoji = sender.titleLabel?.text
@@ -54,6 +40,57 @@ class ViewController: UIViewController {
         present(alertControllerSun, animated: true, completion: nil)
     }
 
+    
+    let customMessageSnow = [
+        "❄️": ["Head to FL and go to the beach", "What are you still doing instide?!", "Have a photoshoot", "Go tubing", "Go shovel that sidewalk! pedestrians are waiting!"]
+    ]
+    
+    @IBAction func showMessageSnow(sender: UIButton){
+        let selectedEmoji = sender.titleLabel?.text
+        let emojiMessageSnow = customMessageSnow[selectedEmoji!]?.randomElement()
+        let alertControllerSnow = UIAlertController(title: "Snowing outside?", message: emojiMessageSnow, preferredStyle: UIAlertController.Style.alert)
+        
+        alertControllerSnow.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertControllerSnow, animated: true, completion: nil)
+    }
+    
+    
+    let customMessageStar = [
+        "💫": ["Go star gazing with some friends", "Have some sister time while star gazing", "Find a place without air polution and look at those beautiful stars", "Don't care about the beautiful meteor shower? go to sleep early!", "Go Gaze At That Night Sky!"]
+    ]
+    @IBAction func showMessageStar(sender: UIButton){
+        let selectedEmoji = sender.titleLabel?.text
+        let emojiMessageStar = customMessageStar[selectedEmoji!]?.randomElement()
+        let alertControllerStar = UIAlertController(title: "The Stars Are Beautiful Tonight!", message: emojiMessageStar, preferredStyle: UIAlertController.Style.alert)
+        
+        alertControllerStar.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertControllerStar, animated: true, completion: nil)
+    }
+    
+    
+    let customMessageRainbow = [
+        "🌈": ["Head out and hunt that pot of gold at the end!"]
+    ]
+    @IBAction func showMessageRainbow(sender: UIButton){
+        let selectedEmoji = sender.titleLabel?.text
+        let emojiMessageRainbow = customMessageRainbow[selectedEmoji!]?.randomElement()
+        let alertControllerRainbow = UIAlertController(title: "WOWWWW A Rainbow!", message: emojiMessageRainbow, preferredStyle: UIAlertController.Style.alert)
+        
+        alertControllerRainbow.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertControllerRainbow, animated: true, completion: nil)
+    }
 
+    
+    let customMessageFire = [
+        "🔥": ["I don't know where you are, but get out.","I cannot help you with this one.", "Stop, drop, and roll?"]
+    ]
+    @IBAction func showMessageFire(sender: UIButton){
+        let selectedEmoji = sender.titleLabel?.text
+        let emojiMessageFire = customMessageFire[selectedEmoji!]?.randomElement()
+        let alertControllerFire = UIAlertController(title: "Weather too hot to handle?!", message: emojiMessageFire, preferredStyle: UIAlertController.Style.alert)
+        
+        alertControllerFire.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertControllerFire, animated: true, completion: nil)
+    }
 }
 
